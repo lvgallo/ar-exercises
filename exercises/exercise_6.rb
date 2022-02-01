5fb6144e09bb741d6e3ebd742bd18168b657a217
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-02-01 19:00:11 +0000

    updating hourly_rates

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -20,9 +20,10 @@ end
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Francis", last_name: "Potato", hourly_rate: 45)
 @store1.employees.create(first_name: "James", last_name: "Bain", hourly_rate: 60)
-@store1.employees.create(first_name: "Gary", last_name: "Jipp", hourly_rate: 40)
+@store1.employees.create(first_name: "Gary", last_name: "Jipp", hourly_rate: 51)
 @store1.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 50)
 
-@store2.employees.create(first_name: "Vasiliy", last_name: "Klimkin", hourly_rate: 38)
-@store2.employees.create(first_name: "Christian", last_name: "Nally", hourly_rate: 35)
+@store2.employees.create(first_name: "Vasiliy", last_name: "Klimkin", hourly_rate: 48)
+@store2.employees.create(first_name: "Christian", last_name: "Nally", hourly_rate: 43)
 @store2.employees.create(first_name: "Dominic", last_name: "Tremblay", hourly_rate: 52)
+
